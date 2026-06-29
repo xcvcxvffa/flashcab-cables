b8e3cf6dd7ac6ec9f0293a3ffda1ac2a7593c4a7
meet <xyz0449911@gmail.com>
2026-06-29 16:49:57 +0530

    Update table headings in LT AB Cable to exactly match the live flashcabcables site format

--- client/update_heading_font2.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/data/productData.js', 'utf8');
-
-// Replace standard font classes with table-heading-text equivalent
-content = content.replace(/font-bold text-\[#1e3a8a\] text-lg/g, 'table-heading-text');
-content = content.replace(/font-bold text-\[#1f6f5f\] text-lg/g, 'table-heading-text');
-
-fs.writeFileSync('src/data/productData.js', content);
-console.log('Replaced custom classes with table-heading-text');
