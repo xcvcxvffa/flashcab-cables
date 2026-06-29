78fb2f5be44ce8a3877604fde1a045eac597d768
meet <xyz0449911@gmail.com>
2026-06-29 16:53:11 +0530

    Update construction details for LT AB Cable to match PDF text and clean layout

--- client/src/data/productData.js
+++ client/src/data/productData.js
@@ -282,9 +282,9 @@ export const productCategories = [
     specs: {
       cableStandard: 'IS 14255:1995',
       construction: {
-        'Phase conductor': '<ul class="list-disc pl-5"><li>Stranded Circular or compacted aluminium Class-2 conductor to IS 8130</li><li>XLPE (Cross linked polyethylene) Insulation</li></ul>',
-        'Messenger conductor': '<ul class="list-disc pl-5"><li>Stranded circular or compacted heat-treated aluminium-magnesium alloy wire to IS 398 (part 4)</li></ul>',
-        'Streetlight conductor': '<ul class="list-disc pl-5"><li>Stranded aluminium conductor Class-2 to IS 8130</li><li>XLPE (Cross linked polyethylene) Insulation</li></ul>'
+        'Phase conductor': '<div class="flex flex-col gap-1"><span>Stranded compacted aluminium conductor to IS 8130 Class 2</span><span>Insulated with in-housed developed compounded XLPE (Cross linked polyethylene)</span></div>',
+        'Messenger conductor': '<div class="flex flex-col gap-1"><span>Stranded circular or compacted heat-treated aluminium-magnesium alloy wire to IS 398 (part 4)</span><span>Insulated with in-housed developed compounded XLPE (if required)</span></div>',
+        'Streetlight conductor': '<div class="flex flex-col gap-1"><span>Stranded aluminium conductor to IS 8130, class 2</span><span>Insulated with in-housed developed compounded XLPE</span></div>'
       },
       salientFeatures: [
         'Stranded Compact Aluminium Conductor',
